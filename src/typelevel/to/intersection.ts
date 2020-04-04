f0212c75f0ffc574a72280cc79a2524e78c6cb7b
yyxi <178086254+yyxi@users.noreply.github.com>
2020-04-04 12:32:57 +0400

    chore: fix tests & workflows

--- src/typelevel/to/intersection.ts
+++ src/typelevel/to/intersection.ts
@@ -1,8 +1,8 @@
 /**
  * Defines an intersection type of all union items.
  */
-export type Intersection<U> = (U extends any
-? (k: U) => void
-: never) extends (k: infer I) => void
+export type Intersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I
+) => void
   ? I
   : never
